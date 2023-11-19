576e8db99bd4b6e0da35d41391fc206b2c695e2c
vox <i.flint.lab@gmail.com>
2023-11-19 19:31:19 +0800

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Flint"
+include(":app")
+include(":flint-core")
