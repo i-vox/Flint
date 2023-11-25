77f03a0aadfc668132ec2679ff1f02e9393c85b1
vox <i.flint.lab@gmail.com>
2023-11-25 20:42:19 +0800

    add sample project

---
+++ app/src/main/java/com/vox/exposure/flint/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.vox.exposure.flint
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        addParts()
+    }
+
+    private fun addParts() {
+
+    }
+}
--- flint-core/src/main/java/com/vox/exposure/flint_core/detector/ActivityDetector.kt
+++ flint-core/src/main/java/com/vox/exposure/flint_core/detector/ActivityDetector.kt
@@ -4,6 +4,11 @@ import android.app.Activity
 import android.app.Application
 import android.os.Bundle
 
+/**
+ * Perceive the life cycle of all activities
+ * As part of the exposure detection trigger
+ * Provide opportunities to clear exposed business
+ */
 internal object ActivityDetector : Application.ActivityLifecycleCallbacks {
     override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
 
